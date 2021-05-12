01d6c7d6348f874c59f9a06865a354617363c207
ad-858 <aarti.dhiman858@gmail.com>
2021-05-12 12:59:04 +0530

    comparison operators and ternary operators

---
+++ comparisonop.js
@@ -0,0 +1,13 @@
+//comparison operator///
+let x=70;
+let y=50;
+let a=30;
+let b=20;
+if (x>y){
+    console.log('greator than operator');
+}
+if(y<x){
+    console.log("y is less than x");
+
+}
+a>b?console.log(x):console.log(y);
