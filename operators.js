c3ee278fe2549260270a35e1eb3b46a500e75685
ad-858 <aarti.dhiman858@gmail.com>
2021-05-12 12:40:33 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,15 @@
+////////////*********arithmatic operators**********************//////
+let x=21;
+let y=10;
+let addition;
+addition=x+y;
+console.log(addition);
+let sub ;
+sub=x-y;
+console.log(sub);
+let multiplication=x*y;
+console.log(multiplication);
+let division=x/y;
+console.log(division);
+let modulusdiv=x%y;
+console.log(modulusdiv)
