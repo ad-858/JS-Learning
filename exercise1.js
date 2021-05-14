d7438bae4b7b2b4a8389b5b57f23599e242adbd6
ad-858 <aarti.dhiman858@gmail.com>
2021-05-14 09:26:12 +0530

    first coding challenge

---
+++ exercise1.js
@@ -0,0 +1,11 @@
+//***********First coding challenge********** */
+let markWeight=78;
+let markHeight=1.69;
+let johnHeight=1.95;
+let johnWeight=92;
+let markBMI=markWeight/(markHeight*markHeight);
+let johnBMI=johnWeight/(johnHeight*johnHeight);
+let markHeigherBMI=markBMI>johnBMI;
+console.log(markBMI);
+console.log(johnBMI);
+console.log(markHeigherBMI);
