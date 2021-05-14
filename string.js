6af3115af2e6430310e4366ae869c311aaf87d79
ad-858 <aarti.dhiman858@gmail.com>
2021-05-14 09:51:11 +0530

    strings and template literals

---
+++ string.js
@@ -0,0 +1,7 @@
+//*****************Strings and template literals********************** */
+let firstName="Mike";
+let job="Programer";
+let birthYear=1990;
+let currentYear=2021;
+console.log(`${firstName} is a ${currentYear-birthYear} years old ${job}`)
+
