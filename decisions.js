09c1f03d256bc9044ccc0b88a6c8221763285e05
ad-858 <aarti.dhiman858@gmail.com>
2021-05-14 11:32:35 +0530

    decision making statements

---
+++ decisions.js
@@ -0,0 +1,10 @@
+//**********************taking decisions If /Else statements*************************** */
+const birthYear=1990;
+const currentYear=2021;
+let age=currentYear-birthYear;
+if(age>=18){
+    console.log(`you are eligible for driving license...`)
+}else{
+
+    console.log(`you are not eligible for driving license...`)
+}
