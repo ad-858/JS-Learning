90a3a3faad5af8aac9735e84120529de9fed6d7c
ad-858 <aarti.dhiman858@gmail.com>
2021-05-20 13:05:03 +0530

    strict operator and loose operator

---
+++ strictopr.js
@@ -0,0 +1,11 @@
+// ***************Strict operator and loose operator************************/
+let age =18;
+if (age===18){
+    console.log("YaY! You are an adult");
+}
+if (age==18){
+    console.log("cool!!!!!!!");
+}
+//******************use of prompt******************* */
+let newAge=prompt("enter your age");
+console.log(age);
