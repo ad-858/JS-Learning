1704c8b5bfae06a46ab1510208c808af2ca9f294
ad-858 <aarti.dhiman858@gmail.com>
2021-06-01 21:50:36 +0530

    exercise3 part 2

--- exerise3.js
+++ exerise3.js
@@ -1,12 +1,27 @@
 //*****************************coding challenge 3**************** */
+// const team1="Dolphin";
+// const team2="Koalas";
+// const aveScoreOfTeam1=(96+108+89)/3;
+// const aveScoreOfTeam2=(96+108+89)/3;
+// if(aveScoreOfTeam1>aveScoreOfTeam2){
+//     console.log(`${team1} is winner`);
+// }
+// else if(aveScoreOfTeam1<aveScoreOfTeam2)
+// {
+//     console.log(`${team2} is winner`);
+// }
+// else if(aveScoreOfTeam1===aveScoreOfTeam2){
+//     console.log(`Both teams  are winner`);
+
+// }
 const team1="Dolphin";
 const team2="Koalas";
-const aveScoreOfTeam1=(96+108+89)/3;
-const aveScoreOfTeam2=(96+108+89)/3;
-if(aveScoreOfTeam1>aveScoreOfTeam2){
+const aveScoreOfTeam1=(97+117+101)/3;
+const aveScoreOfTeam2=(109+95+123)/3;
+if(aveScoreOfTeam1>aveScoreOfTeam2 && aveScoreOfTeam1>=100){
     console.log(`${team1} is winner`);
 }
-else if(aveScoreOfTeam1<aveScoreOfTeam2)
+else if(aveScoreOfTeam1<aveScoreOfTeam2 && aveScoreOfTeam2>=100)
 {
     console.log(`${team2} is winner`);
 }
