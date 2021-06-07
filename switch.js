8ab12c802affc4aa7f431d6800b74f5327b4105f
ad-858 <aarti.dhiman858@gmail.com>
2021-06-07 12:11:03 +0530

    switch statement

---
+++ switch.js
@@ -0,0 +1,10 @@
+// ***********switch statement****************************/
+let age = 18;
+
+switch(age){
+    case (18):
+        console.log("you are 18");
+        break;
+        default:
+            console.log("invalid number")
+}
